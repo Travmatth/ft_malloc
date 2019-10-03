9bc0adb2d5f6ff9c451867929b1a863c63260191
Travis Matthews <tmatthew@e1z2r7p47.42.us.org>
2019-10-02 17:54:20 -0700

    split realloc into multiple funcs

--- src/realloc.c
+++ src/realloc.c
@@ -35,8 +35,26 @@ void	*realloc_chunk(void *pointer, size_t size)
 			, size);
 	}
 	else
-	{
 		DEBUG_LOG("Realloc failed to allocate pointer of size: %zu\n", size);
-	}
 	return (new);
+}
+
+/*
+** Realloc moves contents stored at pointer to a new pointer with space size,
+** or returns a new allocation if pointer is NULL
+*/
+
+void	*realloc(void *pointer, size_t size)
+{
+	if ((size_t)pointer % 8)
+	{
+		DEBUG_LOG("Realloc attempted on misaligned pointer: %p\n", pointer);
+		return (NULL);
+	}
+	else if (pointer == NULL)
+	{
+		DEBUG_LOG("Realloc returning new pointer of size: %zu\n", size);
+		return (malloc(size));
+	}
+	return (realloc_chunk(pointer, size));
 }
